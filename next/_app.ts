7939484385e12b6b935bdcc9d3003329ea723ef8
Rafael do Carmo <rafaelcarmo_15@hotmail.com>
2022-02-02 00:38:56 -0300

    Add files via upload

---
+++ next/_app.ts
@@ -0,0 +1,25 @@
+// import App from "next/app";
+import type { AppProps } from 'next/app'
+
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from 'styles/global'
+import Head from 'next/head'
+import theme from 'styles/theme'
+
+function App({ Component, pageProps }: AppProps) {
+  return 
+  <>
+    <Head>
+      <title>Home Page</title>
+      <link rel="shortcut icon" href="favicon.ico" />
+      <link rel="apple-touch-icon" href="favicon.ico" />
+      <link rel="manifest" href="/manifest.json" />
+      <meta name="description" content="A simple Page" />
+    </Head>
+    <ThemeProvider theme={theme}>
+    <Component {...pageProps} />
+    </ThemeProvider>
+  </>
+}
+
+export default App
